828e34e75b53073b5c6981f1870514fd022554b5
Le Huong <128349225+lehuong0510@users.noreply.github.com>
2024-04-02 09:54:41 +0700

    addDesk

--- app/src/main/java/com/example/flashcat/Activity/Desk/DeskActivity.java
+++ app/src/main/java/com/example/flashcat/Activity/Desk/DeskActivity.java
@@ -1,11 +1,8 @@
 package com.example.flashcat.Activity.Desk;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,45 +13,26 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
 
-import com.example.flashcat.Adapter.DeskFlashcatAdapter;
 import com.example.flashcat.Fragment.DeskFragment;
-import com.example.flashcat.Model.Flashcard;
 import com.example.flashcat.R;
-
-import java.util.ArrayList;
 
 public class DeskActivity extends AppCompatActivity {
     private Toolbar toolbarDesk ;
     private Button btnStudy;
     private ImageButton btnMore;
-    private RecyclerView rcFlashcard;
-    private ArrayList<Flashcard> lstFlashCard;
-    private DeskFlashcatAdapter adapter;
-    private DeskFragment deskFragment = new DeskFragment();;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_desk);
         findID();
+
         // Thêm Fragment vào framelayout container
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_layout_desk, deskFragment)
+                .replace(R.id.fragment_layout_desk, new DeskFragment())
                 .commit();
-
-
 
         setSupportActionBar(toolbarDesk);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        lstFlashCard = new ArrayList<>();
-        lstFlashCard.add(new Flashcard("one","mot"));
-        lstFlashCard.add(new Flashcard("two","hai"));
-        lstFlashCard.add(new Flashcard("three","ba"));
-        lstFlashCard.add(new Flashcard("four","bon"));
-
-        adapter = new DeskFlashcatAdapter(lstFlashCard,this);
-        rcFlashcard.setAdapter(adapter);
-        rcFlashcard.setLayoutManager(new LinearLayoutManager(this));
         btnStudy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,7 +51,7 @@ public class DeskActivity extends AppCompatActivity {
         toolbarDesk = findViewById(R.id.toolbar_Desk);
         btnStudy = findViewById(R.id.btn_Study);
         btnMore = findViewById(R.id.action_more_desk);
-        rcFlashcard= findViewById(R.id.list_flashcard_desk);
+
     }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -105,15 +83,4 @@ public class DeskActivity extends AppCompatActivity {
         popupMenu.show();
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1) {
-            if (resultCode == RESULT_OK) {
-                // Xử lý khi Intent đã trở về từ FlashcardActivity
-                deskFragment.getFlashcardTopAdapter().setStatus(true); // Đặt status thành true khi Intent trở về
-            }
-        }
-
-    }
 }
--- app/src/main/java/com/example/flashcat/Fragment/DeskFragment.java
+++ app/src/main/java/com/example/flashcat/Fragment/DeskFragment.java
@@ -77,8 +77,7 @@ public class DeskFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
         listFlashCard = new ArrayList<>();
-        listFlashCard.add(new Flashcard("one","mot"));
-        listFlashCard.add(new Flashcard("two","hai"));
+
         // Thêm phần tử đặc biệt để đại diện cho layout tương tác
         listFlashCard.add(new Flashcard()); // Phần tử đặc biệt
     }
